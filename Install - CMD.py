7067044b94c526abc83c3300ba58b8e32ab89330
Serj <64781822+AvenCores@users.noreply.github.com>
2021-03-20 22:47:21 +0300

    Update Install - CMD.py

--- Install - CMD.py
+++ Install - CMD.py
@@ -2,11 +2,9 @@ import os
 
 print("Установка библиотек")
 os.system("pip3 install --upgrade pip")
-bib = ["requests", "as"]
+bib = ["requests"]
 for i in range(len(bib)):
-        os.system("pip3 install "+bib[i])
+    print("Установка "+bib[i]+"...\033[0m")
+    os.system("pip3 install "+bib[i])
 os.system('cls')
-
-while True:
-    print = input("\nУстановка завершена!\n\nНажмите ENTER чтобы выйти")
-    exit()
+print("Установка завершена!")
