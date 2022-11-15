d759f48db1bf819bcf6bda42af8d28f80e4a9748
AvenCores <avencores@gmail.com>
2022-11-15 18:27:45 +0300

    V4.9

--- Install-Terminal.py
+++ Install-Terminal.py
@@ -1,10 +1,11 @@
 from os import system,name
 
+system('cls' if name == 'nt' else 'clear')
 print("Установка библиотек")
 system("pip3 install --upgrade pip")
 bib = ["requests", "termcolor"]
 for i in range(len(bib)):
-    print("Установка "+bib[i]+"...\033[0m")
+    print("\n"+"Установка "+bib[i]+"...\033[0m")
     system("pip3 install "+bib[i])
 system('cls' if name == 'nt' else 'clear')
 print("Установка завершена!")
--- Install-Tinker.py
+++ Install-Tinker.py
@@ -1,10 +1,12 @@
 from os import system,name
 
+system('cls' if name == 'nt' else 'clear')
 print("Установка библиотек")
 system("pip3 install --upgrade pip")
 bib = ["requests", "tk"]
 for i in range(len(bib)):
-        system("pip3 install "+bib[i])
+    print("\n"+"Установка "+bib[i]+"...\033[0m")
+    system("pip3 install "+bib[i])
 system('cls' if name == 'nt' else 'clear')
 print("Установка завершена!")
 print("\nНажмите Enter чтобы закрыть")
