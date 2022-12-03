70b4179e567d969b72e99c8dccff75f27796d27b
AvenCores <avencores@gmail.com>
2022-12-03 15:11:45 +0300

    V5.1

--- Build/Build.py
+++ Build/Build.py
@@ -1,4 +1,4 @@
 from os import system
 
-system("pyinstaller --clean -F --uac-admin ..\HZF-Download-Manager-Terminal-V5.0.py")
-system("pyinstaller --clean -F --uac-admin ..\HZF-Download-Manager-Tinker-V5.0.pyw")
+system("pyinstaller --clean -F --uac-admin ..\HZF-Download-Manager-Terminal-V5.1.py")
+system("pyinstaller --clean -F --uac-admin ..\HZF-Download-Manager-Tinker-V5.1.pyw")
